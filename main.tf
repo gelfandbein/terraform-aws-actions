d31d9f0b8192fbd70122499b43c9e966c1f4e216
gelfandbein <boris.gelfandbein@gmail.com>
2020-05-25 10:00:43 +0300

    Added bootstrap

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 # Require TF version to be same as or greater than 0.12.13
 terraform {
   required_version = ">=0.12.13"
-  #backend "s3" {
-  #  bucket         = "kyler-github-actions-demo-terraform-tfstate"
-  #  key            = "terraform.tfstate"
-  #  region         = "eu-central-1"
-  #  dynamodb_table = "aws-locks"
-  #  encrypt        = true
-  #}
+  backend "s3" {
+    bucket         = "github-actions-terraform-tfstate"
+    key            = "terraform.tfstate"
+    region         = "eu-central-1"
+    dynamodb_table = "aws-locks"
+    encrypt        = true
+  }
 }
 
 # Download any stable version in AWS provider of 2.36.0 or higher in 2.36 train
@@ -17,12 +17,10 @@ provider "aws" {
 }
 
 
-/* Commented out until after bootstrap
-
 # Call the seed_module to build our ADO seed info
 module "bootstrap" {
   source                      = "./modules/bootstrap"
-  name_of_s3_bucket           = "kyler-github-actions-demo-terraform-tfstate"
+  name_of_s3_bucket           = "github-actions-terraform-tfstate"
   dynamo_db_table_name        = "aws-locks"
   iam_user_name               = "GitHubActionsIamUser"
   ado_iam_role_name           = "GitHubActionsIamRole"
@@ -60,4 +58,4 @@ resource "aws_route_table" "route_table2" {
     Terraform = "true"
   }
 }
-*/
+
